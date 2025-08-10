b8193b40b92c6596f28f0dba0c2e516b382c06a3
ygonzalez19 <yuliana.gonzalez.chaves@est.una.ac.cr>
2025-08-10 00:54:12 -0600

    cleaning code

--- frontend/src/app/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AppNavigator from '@/navigation/AppNavigator';
-import './global.css'; // Para NativeWind
-
-export default function App() {
-  return <AppNavigator />;
-}
