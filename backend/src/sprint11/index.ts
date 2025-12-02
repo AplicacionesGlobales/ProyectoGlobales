55787a677b9021f1ce6495ca608e1eb3fc4936f1
ericktorres-mw <erick.torres@midware.net>
2025-12-02 14:12:15 -0600

    Netsuite Cabinet Sync Via Cycle

--- backend/src/sprint11/index.ts
+++
@@ -1,5 +0,0 @@
-// src/sprint11/index.ts
-export * from './sprint11.module';
-export * from './sprint11.service';
-export * from './sprint11.controller';
-export * from './dto';
--- frontend/src/components/Appointments/index.ts
+++
@@ -1,6 +0,0 @@
-// components/Appointments/index.ts
-
-export { AppointmentStats } from './AppointmentStats';
-export { AppointmentItem } from './AppointmentItem';
-export { AppointmentFilters } from './AppointmentFilters';
-export { ClientAppointmentHistory } from './ClientAppointmentHistory';
